5cdbaf60545c7cc432c232af720a2207e5b41294
dany <stefanusdany12@gmail.com>
2021-05-15 15:34:02 +0700

    add comment MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,12 +12,12 @@ import com.example.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //deklarasi dan inisiasi kelas
         val homefrag = Home()
         val searchfrag = Search()
         val notificationfrag = Notification()
