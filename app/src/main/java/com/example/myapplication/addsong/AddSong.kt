89ab39cd783d927545289acd1cac69d43a5b7719
dany <stefanusdany12@gmail.com>
2021-05-21 16:21:50 +0700

    updating SetPasswordApp.kt and Notification in settings

--- app/src/main/java/com/example/myapplication/addsong/AddSong.kt
+++ app/src/main/java/com/example/myapplication/addsong/AddSong.kt
@@ -11,7 +11,6 @@ import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.annotation.RequiresApi
